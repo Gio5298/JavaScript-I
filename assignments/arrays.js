23a5d0306bc1fbd7c370bcffd143696f13c08e9c
Regino P <regino5298@gmail.com>
2019-10-14 16:03:41 -0700

    init funct-conv

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -109,8 +109,10 @@ console.log(carYears());
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars = function(){
   let cars = []
-  for (let i = 0; i < 2000; i ++){
-    cars.push(inventory[i][carYears()])
+  for (let i = 0; i < carYears().length; i ++){
+    if(carYears()[i] < 2000) {
+      count++;
+    }
   };
   return cars;
 }
